007409bbf02c741167139f8a9d97a5a71bcf225a
jmwalsh91 <jordan@jmwalsh.dev>
2024-03-04 23:24:00 -0500

    params working

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -10,10 +10,13 @@ import (
 
 func (s *Server) RegisterRoutes() http.Handler {
 	r := httprouter.New()
-	r.HandlerFunc(http.MethodGet, "/", s.HelloWorldHandler)
-
-	r.HandlerFunc(http.MethodGet, "/health", s.healthHandler)
-
+	r.GET("/", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		s.HelloWorldHandler(w, r)
+	})
+	r.GET("/health", func(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
+		s.healthHandler(w, r)
+	})
+	r.GET("/item/:name", s.getItem)
 	return r
 }
 
@@ -34,6 +37,25 @@ func (s *Server) healthHandler(w http.ResponseWriter, r *http.Request) {
 
 	if err != nil {
 		log.Fatalf("error handling JSON marshal. Err: %v", err)
+	}
+
+	_, _ = w.Write(jsonResp)
+}
+
+func (s *Server) getItem(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+	name := ps.ByName("name")
+
+	resp := map[string]string{
+		"item": name,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	jsonResp, err := json.Marshal(resp)
+	if err != nil {
+		log.Printf("Error handling JSON marshal. Err: %v", err)
+		w.WriteHeader(http.StatusInternalServerError)
+		return
 	}
 
 	_, _ = w.Write(jsonResp)
